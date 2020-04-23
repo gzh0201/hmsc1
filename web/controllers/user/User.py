88248e87fb5fa446c6224f7cb812b1d2b7bdedd4
hua <1356284534@qq.com>
2020-04-23 17:35:06 +0800

    登录模块完成

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,4 +1,8 @@
-from flask import Blueprint,render_template,request,jsonify
+from flask import Blueprint,render_template,request,jsonify,make_response
+from common.models.user import User
+from common.libs.user.UserService import UserService
+
+import json
 
 router_user = Blueprint('user_page',__name__)
 
@@ -25,7 +29,32 @@ def login():
         resp['code'] = -1
         resp['msg'] = '请输入正确的密码'
         return jsonify(resp)
-    return jsonify(resp)
+
+    # 从数据库取出user
+    user_info = User.query.filter_by(login_name=login_name).first()
+    print(user_info)
+    # 判断用户名
+    if not user_info:
+        resp['code'] = -1
+        resp['msg'] = '用户名不存在'
+        return jsonify(resp)
+    # 判断密码
+    if user_info.login_pwd != UserService.generatePwd(login_pwd,user_info.login_salt):
+        resp['code'] = -1
+        resp['msg'] = "密码输入错误"
+        return jsonify(resp)
+    
+    # 判断用户状态
+    if user_info.status != 1:
+        resp['code'] = -1
+        resp['msg'] = "用户已被禁用，请联系管理员处理"
+        return jsonify(resp)
+
+    response = make_response(json.dumps({'code':200,"msg":"登录成功~~"}))
+    # Cookie中存入的信息是uid
+    response.set_cookie("hmsc_1901C","%s@%s"%(UserService.generateAuthCode(user_info),user_info.uid),60*60*24*15)
+    
+    return response
 
 @router_user.route('/logout')
 def logout():
